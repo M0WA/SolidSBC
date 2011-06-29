4f65c930b06a65ef07868bf484007850407b90dc
admin <admin@bd7e3521-35e9-406e-9279-390287f868d3>
2011-06-29 11:07:22 +0000

    -solidsbc is now version 1.4
    git-svn-id: file:///root/svn_import/SolidSBC@15 bd7e3521-35e9-406e-9279-390287f868d3

--- SolidSBCSrvSvc/defines.h
+++ SolidSBCSrvSvc/defines.h
@@ -2,7 +2,7 @@
 
 //version
 #define SSBC_SRVSVC_VERSION_MAJOR		_T("1")
-#define SSBC_SRVSVC_VERSION_MINOR		_T("3")
+#define SSBC_SRVSVC_VERSION_MINOR		_T("4")
 
 //windows service releated names
 #define SSBC_SRVSVC_NAME				_T("SolidSbcSrv")
