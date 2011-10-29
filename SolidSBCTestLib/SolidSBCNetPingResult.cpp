0eb54fb243fae0bb03f1d0ffaca0b2025f76be46
admin <admin@bd7e3521-35e9-406e-9279-390287f868d3>
2011-10-29 22:08:32 +0000

    -fix tcp result table
    git-svn-id: file:///root/svn_import/SolidSBC@79 bd7e3521-35e9-406e-9279-390287f868d3

--- SolidSBCTestLib/SolidSBCNetPingResult.cpp
+++ SolidSBCTestLib/SolidSBCNetPingResult.cpp
@@ -4,13 +4,13 @@
 ULONG CSolidSBCNetPingResult::GetDuration(void)
 {
 	ULONG ullValue = 0;
-	GetKeyValue( "rtt_duration", ullValue , (ULONG) 0 );
+	GetKeyValue<ULONG>( "rtt_duration", ullValue , (ULONG) 0 );
 	return ullValue;
 }
 
 ULONGLONG CSolidSBCNetPingResult::GetICMPOpts(void)
 {
 	ULONGLONG ullValue = 0;
-	GetKeyValue( "icmp_opts", ullValue , (ULONGLONG) 0.0 );
+	GetKeyValue<ULONGLONG>( "icmp_opts", ullValue , (ULONGLONG) 0 );
 	return ullValue;
 }
--- SolidSBCTestLib/SolidSBCNetTcpResult.h
+++ SolidSBCTestLib/SolidSBCNetTcpResult.h
@@ -5,7 +5,7 @@
 class CSolidSBCNetTcpResult : CSolidSBCTestResult
 {
 public:
-	CSolidSBCNetTcpResult(void) : CSolidSBCTestResult("testLib_TCPResults") {};
+	CSolidSBCNetTcpResult(void) : CSolidSBCTestResult("testLib_NetTCPResults") {};
 	
 	DOUBLE GetDuration(void);
 	inline void SetDuration(DOUBLE dDuration) { SetKeyValue( "handshake_duration", dDuration); }
