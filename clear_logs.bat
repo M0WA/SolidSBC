5052050f94f57f7730d83e083e0ab6c5787ceb22
admin <admin@bd7e3521-35e9-406e-9279-390287f868d3>
2011-11-13 03:06:48 +0000

    git-svn-id: file:///root/svn_import/SolidSBC@88 bd7e3521-35e9-406e-9279-390287f868d3

---
+++ clear_logs.bat
@@ -0,0 +1,11 @@
+del Win32\Release\ssbc_srv.log
+del Win32\Release\ssbc_cli.log
+
+del Win32\Debug\ssbc_srv.log
+del Win32\Debug\ssbc_cli.log
+
+del x64\Release\ssbc_srv.log
+del x64\Release\ssbc_cli.log
+
+del x64\Debug\ssbc_srv.log
+del x64\Debug\ssbc_cli.log
